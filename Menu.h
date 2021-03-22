557b9e55af2519e8c6f71c2af52cc115be16addf
nvirdi2 <71736420+nvirdi2@users.noreply.github.com>
2021-03-22 19:58:55 -0400

    Update Menu.h

--- Menu.h
+++ Menu.h
@@ -4,19 +4,23 @@
 // Date: March 22, 2021
 
 //I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
-#ifndef SDDS_MENU_H_
-#define SDDS_MENU_H_
+#ifndef SDDS_MENU_H
+#define SDDS_MENU_H
+
+#include <iostream>
+
 namespace sdds {
     class Menu
     {
-      char* m_text; // holds the menu content dynamically
-      int m_noOfSel;  // holds the number of options displayed in menu content
-      void display()const;
-   public:
-      Menu(const char* MenuContent, int NoOfSelections);
-      virtual ~Menu();
-      int& operator>>(int& Selection);
-      // add safe copying logic
+        char* m_text; 
+        int m_noOfSel;  
+        void display()const;
+        void setText(const char*);
+    public:
+        Menu(const char* MenuContent, int NoOfSelections);
+        virtual ~Menu();
+        int& operator>>(int& Selection);
+        Menu(const Menu&);
     };
 }
-#endif // !SDDS_MENU_H_ 
+#endif
