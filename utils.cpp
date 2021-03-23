2b2292d30e71f261fbb03a93d3e5cbe264884ff3
nvirdi2 <71736420+nvirdi2@users.noreply.github.com>
2021-03-22 20:11:38 -0400

    Update utils.cpp

--- utils.cpp
+++ utils.cpp
@@ -5,7 +5,7 @@
 
 //I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
 
-#include <iostream>
+/*#include <iostream>
 #include <string>
 
 #include <cstring>
@@ -165,5 +165,179 @@ namespace sdds
                 inputFromUser = getInt();
             }
         } return inputFromUser;
+    }
+}*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+#include <cstring>
+#include "utils.h"
+#include "Time.h"
+
+using namespace std;
+namespace sdds {
+    bool debug = false;  
+    int getTime() {
+        int mins = -1;
+        if (debug) {
+            Time t(0);
+            cout << "Enter current time: ";
+            do {
+                cin.clear();
+                cin >> t;   
+                if (!cin) {
+                    cout << "Invlid time, try agian (HH:MM): ";
+                    cin.clear();
+                }
+                else {
+                    mins = int(t);
+                }
+                cin.ignore(1000, '\n');
+            } while (mins < 0);
+        }
+
+        else {
+            time_t t = time(NULL);
+            tm lt = *localtime(&t);
+            mins = lt.tm_hour * 60 + lt.tm_min;
+        }
+
+        return mins;
+    }
+
+    int getInt(const char* prompt) {
+        int check = 0;
+        bool num_found = false;
+        int num;
+
+        if (prompt != nullptr)
+            cout << prompt;
+        string line;
+        getline(cin, line);
+        line = line + '\n';
+        bool neg = false;
+
+        while (check == 0 || check == 1) {
+            num = 0;
+            int i = 0;
+            neg = false;
+            num_found = false;
+            while (line[i]) {
+                if (line[i] == '-')
+                    neg = true;
+                else if (line[i] < '0' || line[i] > '9') {
+                    if (num_found == false) {
+                        cout << "Oye Hoye: " << line[i] << endl;
+                        check = 0;
+                    }
+
+                    else if (line[i] == '\n')
+                        check = 2;
+                    else
+                        check = 1;
+                    break;
+                }
+
+                else {
+                    num_found = true;
+                    num = num * 10 + line[i] - '0';
+                }
+
+                i++;
+            }
+
+            if (neg)
+                num = 1 * num;
+            if (check != 2) {
+                if (check == 0)
+                    cout << "Bad integer value, try again: ";
+                else
+                    cout << "Enter only an integer, try again: ";
+                getline(cin, line);
+                line = line + '\n';
+            }
+        }
+
+        return num;
+    }
+
+    int getInt(int min, int max, const char* prompt, const char* errorMessage, bool showRangeAtError) {
+
+        int check = 0;
+        bool num_found = false;
+        int num;
+
+        if (prompt != nullptr)
+            cout << prompt;
+        string line;
+        getline(cin, line);
+        line = line + '\n';
+        bool neg = false;
+
+        while (check == 0 || check == 1) {
+            neg = false;
+            num = 0;
+            int i = 0;
+            num_found = false;
+
+            while (line[i]) {
+                if (line[i] == '-')
+                    neg = true;
+                else if (line[i] < '0' || line[i] > '9') {
+                    if (num_found == false)
+                        check = 0;
+                    else if (line[i] == '\n')
+                        check = 2;
+                    else
+                        check = 1;
+                    break;
+                }
+
+                else {
+                    num_found = true;
+                    num = num * 10 + line[i] - '0';
+                }
+
+                i++;
+            }
+
+            if (neg)
+                num = -1 * num;
+            if (check != 2) {
+                if (check == 0)
+                    cout << "Bad integer value, try again: ";
+                else
+                    cout << "Enter only an integer, try again: ";
+                getline(cin, line);
+                line = line + '\n';
+            }
+
+            else if (num < min || num > max) {
+                check = 1;
+                if (errorMessage != nullptr)
+                    cout << errorMessage;
+                if (showRangeAtError) {
+                    cout << "[" << min << " <= value <= " << max << "]: ";
+                }
+
+                getline(cin, line);
+                line = line + '\n';
+            }
+        }
+        return num;
+    }
+
+    char* getcstr(const char* prompt, std::istream& istr, char delimiter) {
+        if (prompt != nullptr)
+            cout << prompt;
+        string input;
+        getline(istr, input);
+        int len = input.length();
+        char* str = new char[len + 1];
+        strcpy(str, input.c_str());
+        str[len] = '\0';
+        return str;
     }
 }
